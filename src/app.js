cb8185454ff6a5adaf71eccf957bfc213e356a64
Jorge22fv <jorge.22fv@gmail.com>
2022-02-15 21:44:04 +0000

    DomainGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  function domainGenerator(pronounAr, adjAr, nounAr) {
+    let list = document.getElementById("domain");
+    for (let i = 0; i < pronounAr.length; i++) {
+      for (let j = 0; j < adjAr.length; j++) {
+        for (let k = 0; k < nounAr.length; k++) {
+          let result = pronounAr[i] + adjAr[j] + nounAr[k] + ".com";
+          let li = document.createElement("li");
+          let text = document.createTextNode(result);
+          li.appendChild(text);
+          list.appendChild(li);
+        }
+      }
+    }
+  }
+  domainGenerator(pronoun, adj, noun);
 };
